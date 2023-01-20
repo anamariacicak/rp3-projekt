8b19fde24fe5a7036ae0599d4689dbd7688e03ca
anamariacicak <anamariacicak4@gmail.com>
2023-01-20 13:56:46 +0100

    rp3

---
+++ rp3_caffeBar/Receipt.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace rp3_caffeBar
+{
+    partial class Receipt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_idRacuna = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ukupno = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_blagajnikId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Račun br: ";
+            // 
+            // textBox_idRacuna
+            // 
+            this.textBox_idRacuna.Location = new System.Drawing.Point(158, 10);
+            this.textBox_idRacuna.Name = "textBox_idRacuna";
+            this.textBox_idRacuna.Size = new System.Drawing.Size(124, 20);
+            this.textBox_idRacuna.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(31, 48);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(426, 231);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ukupno: ";
+            // 
+            // textBox_ukupno
+            // 
+            this.textBox_ukupno.Location = new System.Drawing.Point(107, 291);
+            this.textBox_ukupno.Name = "textBox_ukupno";
+            this.textBox_ukupno.Size = new System.Drawing.Size(100, 20);
+            this.textBox_ukupno.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Zatvori prikaz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Konobar:";
+            // 
+            // textBox_blagajnikId
+            // 
+            this.textBox_blagajnikId.Location = new System.Drawing.Point(107, 320);
+            this.textBox_blagajnikId.Name = "textBox_blagajnikId";
+            this.textBox_blagajnikId.Size = new System.Drawing.Size(100, 20);
+            this.textBox_blagajnikId.TabIndex = 7;
+            // 
+            // Receipt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 345);
+            this.Controls.Add(this.textBox_blagajnikId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_ukupno);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.textBox_idRacuna);
+            this.Controls.Add(this.label1);
+            this.Name = "Receipt";
+            this.Text = "Receipt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_idRacuna;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ukupno;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_blagajnikId;
+    }
+}
